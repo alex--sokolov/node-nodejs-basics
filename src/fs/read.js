30e8b170afcade760ff46867c8f108479efdf4ee
Alexander Sokolov <thorpe.nulled@gmail.com>
2022-06-05 16:00:27 +0300

    feat: fs/read

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,3 +1,23 @@
+import * as fs from 'fs';
+import * as path from 'path';
+import {fileURLToPath} from 'url';
+import {readFile} from "fs/promises";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const ERROR = 'FS operation failed';
+const filesFolder = 'files';
+const fileName = 'fileToRead.txt';
+const filePath = path.resolve(__dirname, filesFolder, fileName);
+
 export const read = async () => {
-    // Write your code here 
+  try {
+  const readFromFile = await readFile(filePath, {encoding: 'utf-8'});
+  console.log(readFromFile);
+  } catch (err) {
+    if (err.code === 'ENOENT') {
+      throw new Error(ERROR);
+    }
+  }
 };
+
+await read();
