740afd71cc8eda77c85dadb913b2e585252e9de8
Alexander Sokolov <thorpe.nulled@gmail.com>
2022-06-05 18:53:43 +0300

    feat: calcHash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,3 +1,17 @@
+import { createReadStream } from 'fs';
+import {createHash} from 'crypto';
+import {fileURLToPath} from 'url';
+import * as path from 'path';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const fileName = 'files/fileToCalculateHashFor.txt';
+const filePath = path.resolve(__dirname, fileName);
+
 export const calculateHash = async () => {
-    // Write your code here 
+  const hash = createHash('sha256');
+  const input = createReadStream(filePath);
+  input.pipe(hash).setEncoding('hex').pipe(process.stdout);
+
 };
+
+await calculateHash();
