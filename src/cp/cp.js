840b4ba6cc1a7f49ba4a5dadc9f3821348e17125
Alexander Sokolov <thorpe.nulled@gmail.com>
2022-06-05 23:42:12 +0300

    feat: child processes

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,3 +1,15 @@
+import * as path from 'path';
+import {fileURLToPath} from 'url';
+import {fork} from 'child_process';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const scriptFileName = 'files/script.js';
+const scriptFilePath = path.resolve(__dirname, scriptFileName);
+
 export const spawnChildProcess = async (args) => {
-    // Write your code here
-};
+  const child = fork(scriptFilePath, args.slice(2), {stdio: [0, null, null, 'ipc']});
+  child.stdout.on('data', (data) => {
+    console.log('Receive from childs stdout: ', data.toString());
+  });
+}
+await spawnChildProcess(process.argv);
