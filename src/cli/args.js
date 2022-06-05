e06f0ea2c3f3f1d22375017071ab04a7ea278b36
Alexander Sokolov <thorpe.nulled@gmail.com>
2022-06-05 17:18:00 +0300

    feat: cli/args

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,16 +1,24 @@
-import {parseEnv} from "./env.js";
+// Can be checked using this format:
+// npm run cli-args -- --propName value --prop2Name value2
 
 export const parseArgs = () => {
   const args = process.argv.slice(2);
-
-  for (let i = 0; i < args.length; i = i + 2) {
-    console.log(i);
-    console.log(args[i].slice(0, 2));
-    // if (args[i].slice(0,4))
+  const argsArr = [];
+  let i =0;
+  while (i < args.length) {
+    if (args[i].slice(0, 2) === '--') {
+      let curValue = '';
+      let cur = `${args[i]} is undefined`;
+      if (args[i + 1] && args[i + 1].slice(0, 2) !== '--') {
+        curValue = args[i + 1];
+        cur = `${args[i]} is ${curValue}`;
+        i++;
+      }
+      argsArr.push(cur);
+    }
+    i++;
   }
-
-  // const args =
-
+  console.log(argsArr.join(', '));
 };
 
 await parseArgs();
