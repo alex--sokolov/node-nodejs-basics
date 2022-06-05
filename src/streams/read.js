e697616537900f3a1068d1fc306796467114e0f9
Alexander Sokolov <thorpe.nulled@gmail.com>
2022-06-05 19:33:30 +0300

    feat: streams/read

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,3 +1,32 @@
+import * as fs from 'fs';
+import * as path from 'path';
+import {fileURLToPath} from 'url';
+import process from "process";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const ERROR = 'Smth went wrong';
+const filesFolder = 'files';
+const fileName = 'fileToRead.txt';
+
+const filePath = path.resolve(__dirname, filesFolder, fileName);
+
 export const read = async () => {
-    // Write your code here 
+  const newStream = new fs.ReadStream(filePath, {encoding: 'utf-8'});
+
+  newStream.on('readable', function(){
+    let data = newStream.read();
+    if(data != null)
+      process.stdout.write(data);
+  });
+
+  newStream.on('error', function(err){
+    if(err.code == 'ENOENT'){
+      console.log("Файл не найден");
+    }else{
+      console.error(ERROR);
+    }
+  });
+
 };
+
+await read();
