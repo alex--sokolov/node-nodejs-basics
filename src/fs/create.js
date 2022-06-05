9802107925b97b6705dcb62257a4fc711b602fd1
Alexander Sokolov <thorpe.nulled@gmail.com>
2022-06-05 12:08:57 +0300

    feat: fs/create

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,3 +1,13 @@
-export const create = async () => {
-    // Write your code here 
+import * as fs from 'fs';
+
+export const create = async (file, text, error) => {
+
+  fs.writeFile(file, text, {flag: 'wx'}, function (err) {
+    if (err && err.code === 'EEXIST') {
+      throw new Error(error);
+    }
+    console.log('File is created successfully.');
+  });
 };
+
+await create('files/fresh.txt', 'I am fresh and young', 'FS operation failed');
