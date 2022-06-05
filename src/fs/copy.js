266b4813ba5756354295bdfe2e3de902c925b2bd
Alexander Sokolov <thorpe.nulled@gmail.com>
2022-06-05 14:43:46 +0300

    feat: fs/copy

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,3 +1,56 @@
-export const copy = async () => {
-    // Write your code here 
+import * as fs from 'fs';
+import * as path from 'path';
+import {readdir, copyFile} from 'fs/promises';
+import {fileURLToPath} from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const ERROR = 'FS operation failed';
+
+
+export const copy = async (pathFrom, pathTo) => {
+
+  // check if directory exists
+  fs.access(pathTo, async (err) => {
+    if (!err) {
+      console.log('Directory already exists');
+      throw new Error(ERROR);
+    }
+
+    // check if directory to copy from exists
+    await fs.access(pathFrom, async (err) => {
+      if (err) {
+        console.log('There is no directory to copy');
+        throw new Error(ERROR);
+      }
+
+      // create directory
+      fs.mkdir(pathTo, {recursive: true}, (err) => {
+        if (err) throw new Error(ERROR);
+      });
+
+      // read and copy files
+      const readFromDir = await readdir(pathFrom, {withFileTypes: 'true'})
+      readFromDir.forEach((el) => {
+        if (el.isFile()) {
+          try {
+            copyFile(path.resolve(pathFrom, el.name), path.resolve(pathTo, el.name));
+          } catch {
+            console.log(`Error when copying file ${el.name}`);
+            throw new Error(ERROR);
+          }
+        } else if (el.isDirectory) {
+          copy(path.resolve(pathFrom, el.name), path.resolve(pathTo, el.name))
+        }
+      });
+
+      console.log('Directory was successfully copied');
+    });
+  });
 };
+
+
+(async function () {
+  const filesCopyPath = path.resolve(__dirname, 'files-copy')
+  const filesPath = path.resolve(__dirname, 'files')
+  await copy(filesPath, filesCopyPath);
+})()
--- src/fs/create.js
+++ src/fs/create.js
@@ -1,13 +1,16 @@
 import * as fs from 'fs';
 
-export const create = async (file, text, error) => {
+const ERROR = 'FS operation failed';
 
+export const create = async (file, text) => {
   fs.writeFile(file, text, {flag: 'wx'}, function (err) {
-    if (err && err.code === 'EEXIST') {
-      throw new Error(error);
+    if (err) {
+      if (err.code === 'EEXIST') throw new Error(ERROR);
     }
-    console.log('File is created successfully.');
+    else {
+      console.log('File is created successfully.');
+    }
   });
 };
 
-await create('files/fresh.txt', 'I am fresh and young', 'FS operation failed');
+await create('files/fresh.txt', 'I am fresh and young');
