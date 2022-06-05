8a69c845d053d50fc98e49f67285b285050f6c4f
Alexander Sokolov <thorpe.nulled@gmail.com>
2022-06-05 16:44:43 +0300

    feat: cli/env

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,3 +1,16 @@
+import {parseEnv} from "./env.js";
+
 export const parseArgs = () => {
-    // Write your code here 
+  const args = process.argv.slice(2);
+
+  for (let i = 0; i < args.length; i = i + 2) {
+    console.log(i);
+    console.log(args[i].slice(0, 2));
+    // if (args[i].slice(0,4))
+  }
+
+  // const args =
+
 };
+
+await parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,3 +1,17 @@
+// import * as process from 'process';
+
 export const parseEnv = () => {
-    // Write your code here 
-};
+  // console.log(process.env.RSS_value1);
+
+  const rssArr = [];
+  for (const obj in process.env) {
+    if (obj.slice(0,4) === 'RSS_') {
+      rssArr.push(`${obj}=${process.env[obj]}`);
+    }
+  }
+
+  console.log(rssArr.join('; '));
+
+}
+
+parseEnv();
